a821ae8a9a467c2ff6129d7c62fb081c6c978bf9
Павел Галковский <pasha.galkovsky22@gmail.com>
2026-02-19 13:56:04 +0200

    Portal prototype
    Portals first variations

--- StrangePlaces/Assets/_Core/Scripts/Player/Movement.cs
+++ StrangePlaces/Assets/_Core/Scripts/Player/Movement.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [Header("Movement details")]
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private float _gravity = 9f;
 
-    [Header("Camera details")]
     [SerializeField] private float _sensetivity;
 
     private Transform _playerCamer;
@@ -28,15 +26,13 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
-        transform.Rotate(0, Input.GetAxis("Mouse X") * _sensetivity, 0);
+        CameraRotation();
+        PlayerMovement();
 
-        _playerCamer.Rotate(-Input.GetAxis("Mouse Y") * _sensetivity, 0, 0);
+    }
 
-        if (_playerCamer.localRotation.eulerAngles.y != 0)
-        {
-            _playerCamer.Rotate(Input.GetAxis("Mouse Y") * _sensetivity, 0, 0);
-        }
-
+    private void PlayerMovement()
+    {
         moveDirection = new Vector3(Input.GetAxis("Horizontal") * _moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * _moveSpeed);
         moveDirection = transform.TransformDirection(moveDirection);
 
@@ -54,6 +50,17 @@ public class Movement : MonoBehaviour
 
         moveDirection.y -= _gravity * Time.deltaTime;
         _playerController.Move(moveDirection * Time.deltaTime);
+    }
 
+    private void CameraRotation()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * _sensetivity, 0);
+
+        _playerCamer.Rotate(-Input.GetAxis("Mouse Y") * _sensetivity, 0, 0);
+
+        if (_playerCamer.localRotation.eulerAngles.y != 0)
+        {
+            _playerCamer.Rotate(Input.GetAxis("Mouse Y") * _sensetivity, 0, 0);
+        }
     }
 }
---
+++ StrangePlaces/Assets/_Core/Scripts/Portal/PortalView.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PortalView : MonoBehaviour
+{
+    public PortalView otherPortal;
+    public Camera portalView;
+    public Shader portalShader;
+
+    [SerializeField] private MeshRenderer portalMash;
+
+    private Material portalMaterial;
+
+    private void Start()
+    {
+        otherPortal.portalView.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+
+        portalMaterial = new Material(portalShader);
+        portalMaterial.mainTexture = otherPortal.portalView.targetTexture;
+        portalMash.material = portalMaterial;
+    }
+
+    private void Update()
+    {
+        Vector3 lookerPosition = otherPortal.transform.InverseTransformPoint(Camera.main.transform.position);
+        lookerPosition = new Vector3(-lookerPosition.x, lookerPosition.y, -lookerPosition.z);
+        portalView.transform.localPosition = lookerPosition;
+
+        Quaternion diffrence = transform.rotation * Quaternion.Inverse(otherPortal.transform.rotation * Quaternion.Euler(0, 0, 0));
+        portalView.transform.rotation = diffrence * Camera.main.transform.rotation;
+
+        portalView.nearClipPlane = lookerPosition.magnitude;
+    }
+}
